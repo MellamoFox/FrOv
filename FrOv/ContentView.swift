a153c511ba6c62bdce4e813f1b1f43e4a422168c
MellamoFox <tsoi.v87@icloud.com>
2023-05-27 11:33:46 +0500

    Initial Commit

---
+++ FrOv/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  FrOv
+//
+//  Created by MellamoFox on 27.05.2023.
+//
+
+import SwiftUI
+import ExcelReader
+
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FrOv/FrOvApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrOvApp.swift
+//  FrOv
+//
+//  Created by MellamoFox on 27.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct FrOvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
